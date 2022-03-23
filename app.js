1c63bdb91318d08a59d5c931b573db039e391950
bravoosonja <bravoosonja@gmail.com>
2022-03-23 22:53:31 +1100

    update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const container = document.querySelector(".container");
 const userInput = document.getElementById("pixel-size");
 const shakeBtn = document.getElementById("shake-btn");
+const rainbowBtn = document.getElementById("rainbow-btn");
 
 //create grid
 function createGrid() {
-  for (i = 0; i < 28; i++) {
+  for (i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("grids");
     container.appendChild(div);
@@ -40,5 +41,15 @@ shakeBtn.addEventListener("click", function () {
   container.style.setProperty("grid-template-rows", `repeat(16,2fr)`);
   createGrid();
 });
+
+function sketchRainbow() {
+  const rainbowColor = ["#ffbe0b", "#fb5607", "#ff006e", "#8338ec", "#3a86ff"];
+  const randomColor = Math.floor(Math.random() * rainbowColor.length);
+  const square = document.querySelector("div");
+  square.addEventListener("mouseover", function (event) {
+    console.log("rainbow mode");
+    event.target.style.backgroundColor = rainbowColor[randomColor];
+  });
+}
 
 createGrid();
