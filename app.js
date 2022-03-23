f4b3e0cd865561dfe5f1a879d00e614723083bdb
bravoosonja <bravoosonja@gmail.com>
2022-03-23 19:00:21 +1100

    draws on mouseover

--- app.js
+++ app.js
@@ -4,9 +4,41 @@ const shakeBtn = document.getElementById("shake-btn");
 
 //create grid
 function createGrid() {
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < 28; i++) {
     const div = document.createElement("div");
-    div.classList.add("color");
+    div.classList.add("grids");
     container.appendChild(div);
   }
 }
+
+function updateGrid() {
+  container.innerHTML = "";
+  container.style.setProperty(
+    "grid-template-columns",
+    `repeat (${userInput.value},2fr)`
+  );
+
+  for (i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grids");
+    container.appendChild(div);
+  }
+  console.log(userInput.value);
+}
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function (event) {
+  event.target.classList.replace("grids", "color");
+});
+
+userInput.addEventListener("change", updateGrid);
+
+shakeBtn.addEventListener("click", function () {
+  container.innerHTML = "";
+  userInput.value = "";
+  container.style.setProperty("grid-template-columns", `repeat(16,2fr)`);
+  container.style.setProperty("grid-template-rows", `repeat(16,2fr)`);
+  createGrid();
+});
+
+createGrid();
